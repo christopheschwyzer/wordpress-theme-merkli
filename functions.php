66535536f985441734836360e2cb574c17fc9e6a
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-10-27 10:03:31 +0100

    re-add menu

--- functions.php
+++ functions.php
@@ -3,4 +3,18 @@
 add_theme_support('post-thumbnails');
 set_post_thumbnail_size(150, 150); // default Post Thumbnail dimensions
 
+// navigation menu
+function grid_register_main_menus()
+{
+	register_nav_menus(
+		array(
+			'primary-menu' => __('Primary Menu')
+		)
+	);
+}
+
+;
+if (function_exists('register_nav_menus')) add_action('init', 'grid_register_main_menus');
+
+
 ?>
